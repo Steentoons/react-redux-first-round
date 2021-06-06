a24757ef8fb7f8c2912a20ca7318b332488ef0e7
Keyroombar <kirumbaaugustinewambugu@gmail.com>
2021-06-06 15:02:01 +0300

    Initialization and HTML browser set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./css/color.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This color will be changed</h1>
+      <button id="red">RED</button>
+      <button id="blue">BLUE</button>
+      <button id="green">GREEN</button>
+    </div>
+  ); 
+}
+
+export default App;
