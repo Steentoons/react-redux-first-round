53bd0e9bf2c20a4c25e56713943b042a21e27051
Keyroombar <kirumbaaugustinewambugu@gmail.com>
2021-06-07 11:59:02 +0300

    wrapping up and fixing rerendering bugs

--- src/components/Colorize.js
+++ src/components/Colorize.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { store } from "../actions"
+import { useSelector } from "react-redux"
 
 import { colorRed } from "../actions"
 import { colorBlue } from "../actions"
 import { colorGreen } from "../actions"
+
 
 const Colorize = () => {
 
@@ -19,10 +21,14 @@ const Colorize = () => {
         store.dispatch(colorGreen())
     }
 
+    const currentColor = useSelector( state => state.color)
+
+    console.log(currentColor)
+
     return (
         <div>
-        <h1>This is my state </h1>
-        <button id="red" onClick={redClick}>blue</button>
+        <h1 style={{background: currentColor}}>This color changes to the color of the clicked button </h1>
+        <button id="red" onClick={redClick}>RED</button>
         <button id="blue" onClick={blueClick}>BLUE</button>
         <button id="green" onClick={greenClick}>GREEN</button>
     </div>
